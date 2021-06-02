25740fb409953612f5099930f81192f593d2023a
yazantafesh <yazantafesh1@gmail.com>
2021-06-02 17:04:18 +0300

    done

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT;
 
